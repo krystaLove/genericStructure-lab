36b8d6cbd7d2eb432cac043aa7364ffb19105cc1
krystalove <denis.zhuravkov@gmail.com>
2019-11-05 17:46:46 +0200

    - main.c commit

--- main.c
+++ main.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "tVector.h"
 #include "tMatrix.h"
+
 #include "tInteger.h"
-#include "quickSort.h"
+#include "tDouble.h"
 
-void vectorTrial(){
+
+void vectorTrial2(){
     tVector *vec = constructVector(8);
-    int i;
-    for(i = 10; i > 0; i--) insertVector(vec, &i, copyInt);
-    int change = 27;
+    int cnt = 5;
+    fillVector(vec, 7, defaultInt, copyInt);
+    changeValueByPosVector(vec, 2, &cnt, copyInt, copyIntTo);
+    sortVector(vec, swapInt, compareInt);
     printVector(vec, printInt);
-
-    tVector *copy = copyVector(vec, copyInt);
-
-    qsort(vec->array, vec->size, sizeof(void*), compareInt);
-    printVector(vec, printInt);
-
-    quickSort(vec->array, 0, vec->size - 1, swapInt, compareInt);
-    printVector(vec, printInt);
-    printVector(copy, printInt);
 
     freeVector(vec);
 }
-void vectorTrial2(){
-    tVector *vec = constructVector(8);
-}
 void matrixTrial(){
-    tMatrix *matrix = constructMatrix(5);
-    tVector *vec = constructVector(6);
-    int i;
-    for(i = 5; i > 0; i--) insertVector(vec, &i, copyInt);
 
-    addVectorToMatrix(matrix, vec, copyInt);
-    quickSort(vec->array, 0, vec->size - 1, swapInt, compareInt);
-    addVectorToMatrix(matrix, vec, copyInt);
-    addVectorToMatrix(matrix, vec, copyInt);
-    addRowMatrix(matrix);
+    tMatrix *matrix = constructMatrix(5, defaultDouble);
+    tVector *vector = constructVector(8);
+
+    int i;
+    int cnt = 0;
+    int j;
+    for(i = 0; i < 6; i++){
+        for(j = 0; j < 2; j++){
+            double randomValue = 1.0 * (rand()%150) - (rand()%32);
+            insertMatrix(matrix, &randomValue, i, j, copyDouble, copyDoubleTo);
+            cnt++;
+        }
+    }
+    for(i = 0; i < 5; i++){
+        double data = i * 1337;
+        insertVector(vector, &data, copyDouble);
+    }
+    double data = 1337;
+    insertMatrix(matrix, &data, 3, 3, copyDouble, copyDoubleTo);
+    insertMatrix(matrix, &data, 6, 0, copyDouble, copyDoubleTo);
+    sortMatrix(matrix, swapDouble, compareDouble);
+    addVectorToMatrix(matrix, vector, copyDouble);
     printf("%d %d\n", matrix->n, matrix->m);
-    int change = 15;
-    insertMatrix(matrix, &change, 0, 2, copyInt);
-    for(i = 0; i < 10; i++) addVectorToMatrix(matrix, vec, copyInt);
-    printMatrix(matrix, printInt);
+    printMatrix(matrix, printDouble);
+
+    freeMatrix(matrix);
+
 }
 int main() {
     matrixTrial();
-    vectorTrial2();
+    //vectorTrial2();
     return 0;
 }
